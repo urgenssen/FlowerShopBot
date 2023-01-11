0d8c298ae85eecaf26e64a7e9c070aa113a1bab3
Sangdak <82779027+Sangdak@users.noreply.github.com>
2023-01-12 00:58:52 +1000

    Added custom 'User' model in DB.

--- flowershop/flowershopapp/models.py
+++ flowershop/flowershopapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(
+        'Имя пользователя',
+        max_length=200,
+        db_index=True
+    )
+    phoneNumber = models.CharField(
+        'Номер телефона пользователя',
+        unique=True,
+        null=False,
+        blank=False
+    )
+    def __str__(self):
+        return self.full_name
