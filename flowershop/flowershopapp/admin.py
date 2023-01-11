9e39b8a84949a178eb4fe6f9b33d2d1105bea49f
Sangdak <82779027+Sangdak@users.noreply.github.com>
2023-01-12 08:35:03 +1000

    Registered models in 'settings' and 'admin'.

--- flowershop/flowershopapp/admin.py
+++ flowershop/flowershopapp/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User, Category, Bouquet, Order
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    search_fields = ('name', 'phone_number')
+
+@admin.register(Bouquet)
+class BouquetAdmin(admin.ModelAdmin):
+    search_fields = ('name', 'price', 'category')
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    search_fields = ('date_time', 'customer', 'delivery_date_time')
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
