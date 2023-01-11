0252f5b86c269a35806d7fe960fe9b3cd8973a17
kruser <kruser@yandex.ru>
2023-01-11 11:30:34 +0500

    added start steps: event and price

--- flowers_bot.py
+++ flowers_bot.py
@@ -6,27 +6,81 @@ from telegram.ext import (
     MessageHandler, Filters, CallbackContext, Defaults,
 )
 from telegram.constants import PARSEMODE_MARKDOWN_V2
-from random import choice
 from dotenv import load_dotenv
 
 logger = logging.getLogger(__name__)
 
+EVENT_BUTTONS = ['День рождения', 'Свадьба', 'Школа', 'Без повода'] # TODO выбор категорий из базы данных?
+PRICE_BUTTONS = ['~ 500', '~ 1000', '~ 2000', 'Больше', 'Не важно']
+
+def build_menu(buttons, n_cols,
+               header_buttons=None,
+               footer_buttons=None):
+    menu = [buttons[i:i + n_cols] for i in range(0, len(buttons), n_cols)]
+    if header_buttons:
+        menu.insert(0, [header_buttons])
+    if footer_buttons:
+        menu.append([footer_buttons])
+
+    return menu
+
 
 def start(update: Update, context: CallbackContext) -> None:
 
-    # keyboard = [
-    #     ['Новый вопрос', 'Сдаться'],
-    #     ['Мой счет']
-    # ]
-    # reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
+    event_keyboard = build_menu(EVENT_BUTTONS, 2, footer_buttons='Другой повод')
+    reply_markup = ReplyKeyboardMarkup(event_keyboard, resize_keyboard=True)
 
     update.message.reply_text(
         text=(
             '*'
             'К какому событию готовимся?\n'
-            'Выберите один из вариантов, либо укажите свой:\n'
+            'Выберите один из вариантов:\n'
             '*'
         ),
+    reply_markup=reply_markup
+    )
+
+
+def cancel(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text(
+        text=(
+            'До свидания, ждем следующего заказа'
+        ),
+    reply_markup=ReplyKeyboardRemove()
+    )
+
+
+def another_event(update: Update, context: CallbackContext) -> None:
+    pass
+
+
+def send_flower_version(update: Update, context: CallbackContext) -> None:
+
+    price = update.message.text
+    context.user_data['price'] = price
+    event = context.user_data.get('event')
+
+    update.message.reply_text(
+        text=(
+            f'Запрос: \n'
+            f'Категория: {event}\n'
+            f'Цена {price}'
+        ),
+    reply_markup=ReplyKeyboardRemove()
+    )
+
+
+def price_request(update: Update, context: CallbackContext) -> None:
+
+    context.user_data['event'] = update.message.text
+
+    price_keyboard = build_menu(PRICE_BUTTONS, 3)
+    reply_markup = ReplyKeyboardMarkup(price_keyboard, resize_keyboard=True)
+    update.message.reply_text(
+        text=(
+            'На какую сумму рассчитываете?'
+        ),
+    reply_markup=reply_markup
     )
 
 
@@ -48,6 +102,10 @@ if __name__ == '__main__':
     dispatcher = updater.dispatcher
 
     dispatcher.add_handler(CommandHandler('start', start))
+    dispatcher.add_handler(MessageHandler(Filters.text(EVENT_BUTTONS), price_request))
+    dispatcher.add_handler(MessageHandler(Filters.regex('^(Другой повод)$'), another_event))
+    dispatcher.add_handler(MessageHandler(Filters.text(PRICE_BUTTONS), send_flower_version))
+    dispatcher.add_handler(CommandHandler('cancel', cancel))
 
     updater.start_polling()
     updater.idle()
