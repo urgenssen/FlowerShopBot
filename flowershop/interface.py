347f9e2f089f24bbf920055da87751f0040fec45
Sangdak <82779027+Sangdak@users.noreply.github.com>
2023-01-13 10:20:41 +1000

    Started making interface.py

---
+++ flowershop/interface.py
@@ -0,0 +1,20 @@
+from flowershop.flowershopapp.models import User, Bouquet, Category, Order
+
+
+def get_categories_list():
+    return list(Category.objects.all())
+
+
+def is_user_exist(tg_user_id):
+    if User.objects.filter(tg_user_id=tg_user_id).exists():
+        user = User.objects.get(tg_user_id=tg_user_id)
+        return user.tg_user_id, user.phone_number, user.name
+
+
+def add_user(tg_user_id, phone_number, name):
+    User.objects.create(tg_user_id=tg_user_id, name=name, phone_number=phone_number)
+
+
+def get_bouquets_by_filter(category, price):
+    bouquets = Bouquet.objects.filter(categories__name=category).filter(price__lte=price)
+    return bouquets
